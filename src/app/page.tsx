1e3e664c8e9bf0566deeb2a717a022076ccdae9f
lordeadroid <rishabh.rawat@antstack.io>
2025-10-13 12:34:22 +0530

    feat: demo for react compiler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,94 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
+function SlowComponent() {
+  const largeArray = Array.from({ length: 10000 }, (_, i) => i);
+
+  return (
+    <div className="flex flex-wrap overflow-scroll gap-1">
+      {largeArray.map((value) => (
+        <div
+          key={value}
+          className="w-2 h-2 bg-neutral-700"
+          style={{
+            backgroundColor: `rgb(${value % 255}, ${(value * 2) % 255}, ${
+              (value * 3) % 255
+            })`,
+          }}
+        ></div>
+      ))}
+    </div>
+  );
+}
+
+function CounterButton(props: { onClick: () => void }) {
+  return (
+    <button
+      onClick={props.onClick}
+      className="px-4 py-2 bg-neutral-700 text-white rounded hover:bg-neutral-600 border border-white/20"
+    >
+      Increase count
+    </button>
+  );
+}
+
+function ColorPicker(props: {
+  value: string;
+  onChange: (value: string) => void;
+}) {
+  return (
+    <input
+      type="color"
+      value={props.value}
+      onChange={(e) => props.onChange(e.target.value)}
+      className="w-full h-12 cursor-pointer rounded border border-white/20 bg-neutral-700 p-1"
+    />
+  );
+}
+
+function DemoComponent() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("#ffffff");
+
+  return (
+    <div className="flex gap-8">
+      <div className="flex flex-col p-4 border border-white h-64 w-96 gap-4">
+        <h2 className="text-xl font-bold mb-8 text-center">Color Picker</h2>
+        <ColorPicker value={color} onChange={(e) => setColor(e)} />
+        <div className="mt-2">
+          Current value: <br />
+          <span className="font-mono">{color}</span>
+        </div>
+      </div>
+      <div className="flex flex-col p-4 border border-white h-64 w-96 gap-4">
+        <h2 className="text-xl font-bold mb-8 text-center">Counter</h2>
+        <CounterButton onClick={() => setCount((count) => count + 1)} />
+        <div className="mt-2">
+          Current value: <br />
+          <span className="font-mono">{count}</span>
+        </div>
+      </div>
+      <div className="flex flex-col p-4 border border-white h-64 w-96 gap-2">
+        <h2 className="text-xl font-bold text-center">A Slow Component</h2>
+        <span className="text-center text-neutral-200 font-light">
+          (This component renders 10,000 boxes)
+        </span>
+        <SlowComponent />
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="flex flex-col min-h-screen">
+      <h1 className="text-2xl font-bold text-center py-8 absolute top-0 left-0 right-0">
+        React Compiler Demo
+      </h1>
+      <div className={`flex items-center justify-center flex-grow`}>
+        <DemoComponent />
+      </div>
     </div>
   );
 }
